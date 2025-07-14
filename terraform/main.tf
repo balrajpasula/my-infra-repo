9ec35f9be096b3d1a65cf0d070bb7e8b6735a415
Balraj Pasula <157344097+balrajpasula@users.noreply.github.com>
2025-07-14 16:32:45 +0530

    Add custom VPC, subnets, NAT, and EC2 provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,111 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Get default VPC
-data "aws_vpc" "default" {
-  default = true
-}
+# 1. VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-# Get default subnet(s)
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  tags = {
+    Name = "spiderman-vpc"
   }
 }
 
-# Create a security group
+# 2. Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "spiderman-igw"
+  }
+}
+
+# 3. Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "spiderman-public-subnet"
+  }
+}
+
+# 4. Private Subnet (optional for future use)
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "spiderman-private-subnet"
+  }
+}
+
+# 5. Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "spiderman-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# 6. Elastic IP + NAT Gateway for private subnet (optional)
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "spiderman-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# 7. Private Route Table (for future private instances)
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "spiderman-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# 8. Security Group
 resource "aws_security_group" "ec2_sg" {
-  name        = "spiderman_sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = data.aws_vpc.default.id
+  name        = "spiderman-sg"
+  description = "Allow SSH, HTTP, HTTPS"
+  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -29,6 +114,7 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -36,6 +122,7 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
+    description = "HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -48,15 +135,19 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "spiderman-ec2-sg"
+  }
 }
 
-# Launch EC2 instance
+# 9. EC2 Instance in public subnet
 resource "aws_instance" "spiderman_ec2" {
-  ami                         = "ami-03f4878755434977f" # Ubuntu 20.04 for ap-south-1
+  ami                         = "ami-03f4878755434977f" # Ubuntu 20.04 in ap-south-1
   instance_type               = "t2.micro"
-  key_name                    = "spidermankeypair"
+  subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  subnet_id                   = data.aws_subnets.default.ids[0]
+  key_name                    = "spidermankeypair"
   associate_public_ip_address = true
 
   tags = {
@@ -64,7 +155,8 @@ resource "aws_instance" "spiderman_ec2" {
   }
 }
 
+# 10. Output public IP
 output "public_ip" {
-  value = aws_instance.spiderman_ec2.public_ip
+  value       = aws_instance.spiderman_ec2.public_ip
   description = "Public IP of the EC2 instance"
 }
